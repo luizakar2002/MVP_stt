2732eee5b53464d162ba9c101d639dccaeb8d659
luizakar2002 <51238018+luizakar2002@users.noreply.github.com>
2023-09-14 11:58:11 +0400

    Update LeftColumn.js

--- src/components/LeftColumn.js
+++ src/components/LeftColumn.js
@@ -27,31 +27,32 @@ function LeftColumn({ handleUserInputChange }) {
   };
 
   return (
-    <Paper sx={{ flex: 1, padding: '20px', height: '985px', marginBottom: '35px', marginTop: '35px',marginRight: '40px', marginLeft: '40px',borderRadius: 5 }}>
+    <Paper sx={{ flex: 1, padding: '20px', height: '700px', marginBottom: '35px', marginTop: '35px',marginRight: '40px', marginLeft: '40px',borderRadius: 5 }}>
       <TextField
         label="Report"
         multiline
-        rows={37}
+        rows={25}
         fullWidth
         variant="outlined"
         value={fetchedText}
         disabled
+        color="primary"
       />
-      <img
+      {/* <img
         src={edit}
         alt="Icon"
-        style={{ width: '40px', height: '40px', marginLeft: '350px', marginTop: '10px' }}
-      />
+        style={{ width: '40px', height: '40px', marginLeft: '35px', marginTop: '45px', marginRight: "10px" }}
+      /> */}
       <Button type="submit" color="primary" style={{ width: "100px", marginTop: "45px", border: '2px solid #576db6', marginRight: '15px', color: "#4357a3" }}>
         Cancel
       </Button>
-      <Button variant="contained" type="submit" color="primary" style={{ width: "100px", backgroundColor: '#4357a3', marginTop: "45px", border: '2px solid #4357a3' }}>
+      <Button variant="contained" type="submit" color="primary" style={{ width: "100px", backgroundColor: '#4357a3', marginTop: "45px", marginLeft: "20px", border: '2px solid #4357a3' }}>
         Save
       </Button>
-      <Button variant="contained" type="button" color="primary" style={{ backgroundColor: '#4357a3', marginTop: "10px", border: '2px solid #4357a3' }} onClick={fetchTextFromEndpoint}>
+      <Button variant="contained" type="button" color="primary" style={{ backgroundColor: '#4357a3', marginTop: "45px", marginLeft: "20px", border: '2px solid #4357a3' }} onClick={fetchTextFromEndpoint}>
         Get audio text
       </Button>
-      <Button variant="contained" type="button" color="primary" style={{ backgroundColor: '#4357a3', marginTop: "10px", border: '2px solid #4357a3' }} onClick={handleSetUserInput}>
+      <Button variant="contained" type="button" color="primary" style={{ backgroundColor: '#4357a3', marginTop: "45px", marginLeft: "20px", border: '2px solid #4357a3' }} onClick={handleSetUserInput}>
         Set audio text
       </Button>
     </Paper>
